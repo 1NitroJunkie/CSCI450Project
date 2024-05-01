e9cfac1b350bf393ce89d94e4ae125f0a6de2207
Joseph White <71468239+1NitroJunkie@users.noreply.github.com>
2024-04-30 21:35:13 -0500

    Removed email functions

--- submit_form.php
+++ submit_form.php
@@ -1,35 +1,35 @@
 <?php
 
-require 'vendor/autoload.php';  // This path assumes that this script is in the root folder of your project
+//require 'vendor/autoload.php';  // This path assumes that this script is in the root folder of your project
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+//use PHPMailer\PHPMailer\PHPMailer;
+//use PHPMailer\PHPMailer\Exception;
 
-function sendEmailNotification($recipientEmail, $subject, $body) {
-    $phpmailer = new PHPMailer();
-    $phpmailer->isSMTP();
-    $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
-    $phpmailer->SMTPAuth = true;
-    $phpmailer->Username = 'ca3320f4608c9c';
-    $phpmailer->Password = 'becb0b7b6c8e1a';
-    $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Optional: if you want encryption
-    $phpmailer->Port = 2525;
-    
-    $phpmailer->setFrom('josewh2@regent.edu', 'STEM Summer Camp'); // Sender's email address and name
-    $phpmailer->addAddress($recipientEmail); // Add a recipient
-    
-    $phpmailer->isHTML(true); // Set email format to HTML
-    $phpmailer->Subject = $subject;
-    $phpmailer->Body    = $body;
-    $phpmailer->AltBody = strip_tags($body);
-    
-    if(!$phpmailer->send()) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $phpmailer->ErrorInfo;
-    } else {
-        echo 'Message has been sent';
-    }
-  }
+//function sendEmailNotification($recipientEmail, $subject, $body) {
+//    $phpmailer = new PHPMailer();
+//    $phpmailer->isSMTP();
+//    $phpmailer->Host = 'sandbox.smtp.mailtrap.io';
+//    $phpmailer->SMTPAuth = true;
+//    $phpmailer->Username = 'ca3320f4608c9c';
+//    $phpmailer->Password = 'becb0b7b6c8e1a';
+//    $phpmailer->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // Optional: if you want encryption
+//    $phpmailer->Port = 2525;
+//    
+//    $phpmailer->setFrom('josewh2@regent.edu', 'STEM Summer Camp'); // Sender's email address and name
+//    $phpmailer->addAddress($recipientEmail); // Add a recipient
+//    
+//    $phpmailer->isHTML(true); // Set email format to HTML
+//    $phpmailer->Subject = $subject;
+//    $phpmailer->Body    = $body;
+//    $phpmailer->AltBody = strip_tags($body);
+//    
+//    if(!$phpmailer->send()) {
+//        echo 'Message could not be sent.';
+//        echo 'Mailer Error: ' . $phpmailer->ErrorInfo;
+//    } else {
+//        echo 'Message has been sent';
+//    }
+//  }
 
 
 $host = "den1.mysql2.gear.host";
@@ -67,13 +67,13 @@ $dateofregistration = date("Y-m-d H:i:s");  // Current date and time
 if ($stmt->execute()) {
   echo "New records created successfully";
 
-  if ($statusofregistration == 'paid') {
-      sendEmailNotification($email, 'Payment Confirmation', 'Your payment has been confirmed, and your camp spot is secured!');
-  } elseif ($statusofregistration == 'registered') {
-      sendEmailNotification($email, 'Registration Confirmation', 'Thank you for registering for the STEM Summer Camp! We will update you on your payment status.');
-  } elseif ($statusofregistration == 'waiting list') {
-      sendEmailNotification($email, 'Waitlist Notification', 'You have been placed on the waiting list. We will contact you if a spot becomes available.');
-  }
+  //if ($statusofregistration == 'paid') {
+  //    sendEmailNotification($email, 'Payment Confirmation', 'Your payment has been confirmed, and your camp spot is secured!');
+  //} elseif ($statusofregistration == 'registered') {
+  //    sendEmailNotification($email, 'Registration Confirmation', 'Thank you for registering for the STEM Summer Camp! We will update you on your payment status.');
+  //} elseif ($statusofregistration == 'waiting list') {
+  //    sendEmailNotification($email, 'Waitlist Notification', 'You have been placed on the waiting list. We will contact you if a spot becomes available.');
+  //}
 } else {
   echo "Error: " . $stmt->error;
 };
